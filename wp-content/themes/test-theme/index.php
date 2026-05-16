78cc02a973534bb5cde8e8d6a4d09f21634abb9b
Your Name <hardic@axioned.com>
2026-05-16 17:37:53 +0530

    updated test theme

--- wp-content/themes/test-theme/index.php
+++ wp-content/themes/test-theme/index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Azure CI/CD Working";
+get_header();
+?>
+
+<div style="padding:40px;text-align:center;">
+    <h1>Azure GitHub CI/CD Working 🚀</h1>
+    <p>This was deployed automatically from GitHub Actions.</p>
+</div>
+
+<?php
+get_footer();
+?>
